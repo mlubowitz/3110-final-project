0459d6bec8e18da577400546e3f2d781ef000a64
Chris Kim <cjk246@cornell.edu>
2021-10-11 16:46:30 -0400

    Added bin/main.ml for actual gameplay - also added dune file for bin/main.ml

---
+++ bin/main.ml
@@ -0,0 +1,5 @@
+(* File for running the actual game. *)
+
+(* Call board_init, open GUI/ANSiTerminal?,
+ ask for input, check legality, move piece, flip, ask for input, keep track of 
+ score and keep alternating, etc*)
