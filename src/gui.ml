b70e79803de4e5f07c9c03503673b74f951327e5
Vishaak Raghupathy <vr258@cornell.edu>
2021-10-19 11:39:01 -0400

    Images folder and GUI instructions

--- src/gui.ml
+++ src/gui.ml
@@ -12,14 +12,22 @@ let rec draw_matrix_rows x y s row bl =
 match row with
 | 1 -> color_rec x y s bl;
 | _ -> color_rec x y s bl;
-      draw_matrix_rows (x + s) (y) (s) (row - 1) (if bl then false else true);;
+      draw_matrix_rows (x + s) (y) (s) (row - 1) 
+      (if bl then false else true);;
 
 let rec draw_matrix x y s row col bl = 
 match col with 
 | 1 -> draw_matrix_rows x y s row bl;
 | _ -> draw_matrix_rows x y s row bl ;
-       draw_matrix x (y + s) s row (col - 1) (if bl then false else true);;
+       draw_matrix x (y + s) s row (col - 1) 
+       (if bl then false else true);;
+
+let words loc_x loc_y phrase s = 
+  moveto loc_x loc_y;
+  set_color black;
+  draw_string phrase;;
 let init  = 
   open_graph " 800x800";
   set_window_title "Chess Game";
-  draw_matrix 50 50 80 8 8 true;;
+  draw_matrix 50 50 80 8 8 true;
+  words 50 700 "CHESS GAME" 20;;
--- src/gui.mli
+++ src/gui.mli
@@ -4,7 +4,8 @@ val color_rec : int -> int -> int -> bool -> unit;;
 (** [color_rec] draws a rectangle that is either dark or light *)
 
 val draw_matrix : int -> int -> int -> int -> int -> bool -> unit;;
-(** [draw_matrix_rows] draws a matrix with a specified number of rows and columns *)
+(** [draw_matrix_rows] draws a matrix with a 
+     specified number of rows and columns *)
 
 val init : unit;;
 (** [init] creates a window and initializes board*)
