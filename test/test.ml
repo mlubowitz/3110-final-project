bd912b7290aec46f3572adeb841efc8b3826fbf5
Chris Kim <cjk246@cornell.edu>
2021-10-22 17:54:00 -0400

    added board tests

--- test/test.ml
+++ test/test.ml
@@ -3,9 +3,6 @@ open Chess
 open Board
 open Pieces
 
-(* let piece_location_test name piece expected = name >:: fun _ ->
-   assert_equal expected (piece_location piece) *)
-
 let is_legal_test name ori_loc new_loc expected =
   name >:: fun _ -> assert_equal expected (is_legal ori_loc new_loc)
 
@@ -13,7 +10,37 @@ let is_legal_tests = []
 
 let pieces_tests = []
 
-let board_tests = []
+(* ===============board tests below================================= *)
+let get_str_piece_test name board grid expected =
+  name >:: fun _ -> assert_equal expected (get_str_piece board grid)
+
+let get_row_test name board row_num expected =
+  name >:: fun _ -> assert_equal expected (get_row board row_num)
+
+let test_board = init_board ()
+
+let board_tests =
+  [
+    get_str_piece_test "test row 0" test_board (0, 0) "[ ]";
+    get_str_piece_test "test row 1" test_board (1, 5) "[P]";
+    get_str_piece_test "test row 1" test_board (1, 2) "[P]";
+    get_str_piece_test "test row 2" test_board (2, 5) "[ ]";
+    get_str_piece_test "test row 3" test_board (3, 5) "[ ]";
+    get_str_piece_test "test row 4" test_board (4, 5) "[ ]";
+    get_str_piece_test "test row 4" test_board (4, 0) "[ ]";
+    get_str_piece_test "test row 5" test_board (5, 3) "[ ]";
+    get_str_piece_test "test row 6" test_board (6, 7) "[p]";
+    get_str_piece_test "test row 6" test_board (6, 6) "[p]";
+    get_str_piece_test "test row 7" test_board (7, 7) "[ ]";
+    (* Violate encapsulation? Should not know what strings look like?
+       But it is printed out in our terminal representation *)
+    get_row_test "test row 7" test_board 7
+      [ "[ ]"; "[ ]"; "[ ]"; "[ ]"; "[ ]"; "[ ]"; "[ ]"; "[ ]" ];
+    get_row_test "test row 6" test_board 6
+      [ "[p]"; "[p]"; "[p]"; "[p]"; "[p]"; "[p]"; "[p]"; "[p]" ];
+    get_row_test "test row 1" test_board 1
+      [ "[P]"; "[P]"; "[P]"; "[P]"; "[P]"; "[P]"; "[P]"; "[P]" ];
+  ]
 
 let tests =
   "test suite for Chess" >::: List.flatten [ pieces_tests; board_tests ]
