3081df5f89eed05bcef62ea507a3472ad9f31e03
Chris Kim <cjk246@cornell.edu>
2021-10-24 19:40:41 -0400

    Added tests for is_path_empty to test/test.ml Max fixed horizontal_move in state.ml

--- test/test.ml
+++ test/test.ml
@@ -2,6 +2,7 @@ open OUnit2
 open Chess
 open Board
 open Pieces
+open State
 
 let is_legal_test name ori_loc new_loc expected =
   name >:: fun _ -> assert_equal expected (is_legal ori_loc new_loc)
@@ -47,17 +48,11 @@ let board_tests =
   ]
 
 (* ===============state tests below================================= *)
-let v_path_empty_test name state ori_loc new_loc expected =
+let is_path_empty_test name state ori_loc new_loc expected =
   name >:: fun _ ->
-  assert_equal expected (vertical_path_empty state ori_loc new_loc)
-
-let h_path_empty_test name state ori_loc new_loc expected =
-  name >:: fun _ ->
-  assert_equal expected (horizontal_path_empty state ori_loc new_loc)
-
-let dia_path_empty_test name state ori_loc new_loc expected =
-  name >:: fun _ ->
-  assert_equal expected (diagonal_path_empty state ori_loc new_loc)
+  assert_equal expected
+    (is_path_empty state ori_loc new_loc)
+    ~printer:string_of_bool
 
 (* Test state *)
 let test_st = get_state test_board
@@ -66,41 +61,42 @@ let state_tests =
   [
     (* vertical_path_empty, horizonantal_path_empty,vertical_path_empty
        returns true if nothing in between *)
-    v_path_empty_test "vert-nothing in btwn-going up" test_st (5, 7)
+    is_path_empty_test "vert-nothing in btwn-going up" test_st (5, 7)
       (3, 7) true;
-    v_path_empty_test "vert-nothing in btwn-going down" test_st (1, 5)
+    is_path_empty_test "vert-nothing in btwn-going down" test_st (1, 5)
       (4, 5) true;
-    v_path_empty_test "vert-piece in btwn-going up" test_st (7, 7)
+    is_path_empty_test "vert-piece in btwn-going up" test_st (7, 7)
       (3, 7) false;
-    v_path_empty_test "vert-piece in btwn-going down" test_st (0, 3)
+    is_path_empty_test "vert-piece in btwn-going down" test_st (0, 3)
       (4, 3) false;
-    h_path_empty_test "hori-piece in btwn-going left" test_st (7, 7)
+    is_path_empty_test "hori-piece in btwn-going left" test_st (7, 7)
       (7, 5) false;
-    h_path_empty_test "hori-piece in btwn-going right" test_st (1, 1)
+    is_path_empty_test "hori-piece in btwn-going right" test_st (1, 1)
       (1, 5) false;
-    h_path_empty_test "hori-nothing in btwn-going right" test_st (5, 2)
+    is_path_empty_test "hori-nothing in btwn-going right" test_st (5, 2)
       (5, 6) true;
-    h_path_empty_test "hori-nothing in btwn-going left" test_st (5, 5)
+    is_path_empty_test "hori-nothing in btwn-going left" test_st (5, 5)
       (5, 0) true;
-    dia_path_empty_test "dia-nothing in btwn-going NE" test_st (6, 1)
+    is_path_empty_test "dia-nothing in btwn-going NE" test_st (6, 1)
       (3, 4) true;
-    dia_path_empty_test "dia-nothing in btwn-going NW" test_st (6, 7)
+    is_path_empty_test "dia-nothing in btwn-going NW" test_st (6, 7)
       (5, 6) true;
-    dia_path_empty_test "dia-nothing in btwn-going SE" test_st (1, 2)
+    is_path_empty_test "dia-nothing in btwn-going SE" test_st (1, 2)
       (3, 4) true;
-    dia_path_empty_test "dia-nothing in btwn-going SW" test_st (1, 2)
+    is_path_empty_test "dia-nothing in btwn-going SW" test_st (1, 2)
       (3, 1) true;
-    dia_path_empty_test "dia-piece in btwn-going NE" test_st (7, 0)
+    is_path_empty_test "dia-piece in btwn-going NE" test_st (7, 0)
       (2, 5) false;
-    dia_path_empty_test "dia-piece in btwn-going NW" test_st (7, 5)
+    is_path_empty_test "dia-piece in btwn-going NW" test_st (7, 5)
       (5, 3) false;
-    dia_path_empty_test "dia-piece in btwn-going SW" test_st (0, 7)
+    is_path_empty_test "dia-piece in btwn-going SW" test_st (0, 7)
       (5, 2) false;
-    dia_path_empty_test "dia-piece in btwn-going SE" test_st (0, 2)
-      (3, 3) false;
+    is_path_empty_test "dia-piece in btwn-going SE" test_st (0, 2)
+      (3, 5) false;
   ]
 
 let tests =
-  "test suite for Chess" >::: List.flatten [ pieces_tests; board_tests ]
+  "test suite for Chess"
+  >::: List.flatten [ pieces_tests; board_tests; state_tests ]
 
 let _ = run_test_tt_main tests
