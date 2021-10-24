0b012ddcbc335383955df649fd83b67c20d4cfcf
Chris Kim <cjk246@cornell.edu>
2021-10-24 19:10:03 -0400

    updated state.mli for is_path_empty

--- src/state.mli
+++ src/state.mli
@@ -15,7 +15,7 @@ val what_piece : t -> int * int -> Pieces.piece
 (**[what_piece st l] is the piece at location [l] in state [st]. Raises
    [InvalidLocation l] if [l] is not in the board. *)
 
-(*val state_blocked:*)
+val is_path_empty : t -> int * int -> int * int -> bool
 
 (* Update function is unnecessary - just call get_state *)
 (* val update_loc : t -> int * int -> Pieces.piece -> t (** [update_loc
