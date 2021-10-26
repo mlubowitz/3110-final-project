7fccc6ab7b1e3f020ec0e3fa47bde3460a04d4a4
Chris Kim <cjk246@cornell.edu>
2021-10-26 02:04:40 -0400

    Added comments to bin/main.ml and tried to organize/clean up for ease of understanding. -renamed some identifiers to better represent what they are bound to -renamed some functions to better represent what they do Updated is_legal type in pieces.mli to avoid type mismatch error

--- bin/main.ml
+++ bin/main.ml
@@ -3,25 +3,32 @@ open Board
 open State
 open Pieces
 
-(* To play type "make play" in terminal.*)
+(* To play type "make play" in terminal once navigating to root
+   directory.*)
 
-(*Converts str "(0,0)" to a tuple (0,0). *)
+(* ====================START HELPER FUNCTIONS======================== *)
+(* ================================================================== *)
+
+(*Converts str version of tuple to a regular tuple. Example: ["(3,5)"
+  becomes (3,5)]. *)
 let str_to_grid str =
   (int_of_char str.[1] - 48, int_of_char str.[3] - 48)
 
-(* ==================================================================== *)
+(* ======================================================= *)
 
-(* going to be difficult with move_piece if this doesn't return a
-   location *)
-(* is the destination location that piece at input can move to *)
-let rec can_move brd state input =
+(* [get_dest brd state sel_pce_loc] is the destination location that the
+   user wants to move their selected piece to. This function calls
+   is_legal and is_path_empty to determine if the selected piece is able
+   to move to the dest location the player inputs. If illegal move,
+   player is prompted for a different dest location, and this repeats
+   until valid dest is inputted.*)
+let rec get_dest state sel_pce_loc =
   let dest = read_line () |> str_to_grid in
-  let f_piece = what_piece state input in
-  let dest_piece = what_piece state dest in
-
+  let select_piece = what_piece state sel_pce_loc in
+  let pce_on_dest = what_piece state dest in
   match
-    (*check if path blocked or move not legal*)
-    is_legal f_piece dest_piece && is_path_empty state input dest
+    is_legal select_piece pce_on_dest
+    && is_path_empty state sel_pce_loc dest
   with
   | true -> dest
   | false ->
@@ -30,12 +37,15 @@ let rec can_move brd state input =
           "Your selected piece cannot move to that location. Input a \
            new destination location."
       in
-      can_move brd state input
+      get_dest state sel_pce_loc
 
-(* ==================================================================== *)
+(* ======================================================= *)
 
-(* checks if input location has a piece *)
-let rec get_input state =
+(* [get_select_pce_loc state] is the location of the piece that the
+   player wants to select. If player selects a location without a piece,
+   this function keeps asking for a board location until the user inputs
+   a location with a piece on it.*)
+let rec get_select_pce_loc state =
   let input = read_line () |> str_to_grid in
   let piece = what_piece state input in
   match no_piece piece with
@@ -45,58 +55,92 @@ let rec get_input state =
         print_endline
           "Not a piece. Input location of piece you want to select."
       in
-      get_input state
+      get_select_pce_loc state
 
-(* ==================================================================== *)
+(* ================================================================== *)
+(* =======================END HELPER FUNCTIONS======================= *)
 
+(* THE MAIN FUNCTION FOR GAMEPLAY. Comments hopefully help reveal what's
+   going on. *)
 let rec play_game brd state =
+  (* Print current board *)
   let () = print_endline "Current board:" in
   let () = print_board brd in
-  (* prompt for choosing piece player wants to move *)
+
+  (* Prompt player to select the piece they want to move by inputting
+     the piece's location. *)
   let () =
     print_endline
-      "Input the location of the PIECE YOU WANT TO MOVE in EXACT \
-       format (row,column). Upper left is (0,0) and bottom right is \
-       (7,7). NO SPACES!"
-  in
-  (* get the location of the piece player wants to move. If location has
-     no piece, keep asking until player inputs location with piece. *)
-  let input = get_input state in
-  (*this could be a piece type*)
-  (* prompt for destination location *)
-  let () =
-    print_endline
-      "Input the location TO WHICH YOU WANT TO MOVE that piece in \
+      "Select piece you want to move by inputting its location in \
        EXACT format (row,column). Upper left is (0,0) and bottom right \
        is (7,7). NO SPACES!"
   in
-  (* get a destination location - check to see if move can be made from
-     starting to destination location; if not, keep asking until valid
-     location is added *)
-  let dest = can_move brd state input in
-  (* move piece and update board *)
-  let brd = move_piece brd input dest in
-  (* mark the piece as having moved at least once *)
-  let moved_piece = what_piece state input |> first_move in
-  (* the state must be updated so state reflects current board *)
+
+  (* Get the location of the piece player wants to move. If location has
+     no piece, keep asking until player inputs location with piece. *)
+  let select_pce_loc = get_select_pce_loc state in
+
+  (* Prompt for destination location. *)
+  let () =
+    print_endline
+      "Input destination location of selected piece in EXACT format \
+       (row,column). Upper left is (0,0) and bottom right is (7,7). NO \
+       SPACES!"
+  in
+  (* Get the destination location - ask player for dest location; check
+     to see if the selected piece can be legally moved from starting to
+     dest location; if not, keep asking for a dest location until a
+     valid dest location for the selected piece is inputted.*)
+  let dest = get_dest state select_pce_loc in
+
+  (* Move the piece the player selected to position dest. *)
+  let brd = move_piece brd select_pce_loc dest in
+
+  (* Get the moved piece and mark it as having moved at least once. *)
+  let moved_piece = what_piece state select_pce_loc |> first_move in
+
+  (* Update the state so it reflects the updated board. There needs to
+     be 2 function calls to fully update state after a move. The first
+     call to update_loc connects the dest location to moved_piece in the
+     assoc list. The second call to update_loc puts a None piece on the
+     original spot of moved_piece. *)
   let state = update_loc state dest moved_piece in
-  let state = update_loc state input (to_piece input "[ ]") in
+  let state =
+    update_loc state select_pce_loc (to_piece select_pce_loc "[ ]")
+  in
+
+  (* Tell player that move is complete and print the updated board. *)
   let () = print_endline "Move complete:" in
   let () = print_board brd in
+
+  (* COULD CALL A FUNCTION AT THIS POINT TO CHECK FOR CHECKMATE. IF
+     CHECKMATE, THEN THE GAME WOULD STOP AND A WINNER WOULD BE PRINTED
+     OUT AT THIS POINT. *)
+
+  (* "Flip" board orientation so reading it is easy for opposing player.
+     Print this board out. In addition, the state must be "flipped" to
+     accurately reflect what piece is on which board location.*)
   let brd = flip brd in
   let state = flip_state state in
   let () = print_endline "" in
   let () = print_endline "Next player - board flipped: " in
   let () = print_board brd in
+
+  (* Ask if players want to keep playing. *)
   let () =
     print_endline "";
     print_endline "Keep playing? y or n"
   in
   let keep_play = read_line () |> String.lowercase_ascii in
-  if keep_play = "y" then play_game brd state
-  else print_endline "Goodbye"
+  if keep_play = "y" then
+    (*If players want to continue, call play_game with the current board
+      and state.*)
+    play_game brd state
+    (*If players want to stop, print a end of game message and allow
+      play_game to terminate. *)
+  else print_endline "Goodbye!"
 
-(* ==================================================================== *)
+(* ================================================================== *)
 
 let main () =
   ANSITerminal.print_string [ ANSITerminal.blue ]
@@ -105,4 +149,7 @@ let main () =
   let state = init_state board in
   play_game board state
 
+(* Starts game. *)
 let () = main ()
+
+(*CODE ABOVE DOES NOT HANDLE WINNING THE GAME. *)
--- src/pieces.mli
+++ src/pieces.mli
@@ -14,7 +14,7 @@ val to_piece : int * int -> string -> piece
 (** [to_piece s] is the conversion of a piece on the matrix to a piece
     type.*)
 
-val is_legal : piece -> int * int -> bool
+val is_legal : piece -> piece -> bool
 (** [is_legal piece ori_loc new_loc] is the legality of the movement of
     a given piece [piece] from [ori_loc] to [new_loc]. Raises
     [Illegal s] if there is no piece at [ori_loc]*)
