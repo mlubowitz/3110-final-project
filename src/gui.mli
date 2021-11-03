8a9b148416f82a466659aae2387d1571e29cb54b
Vishaak Raghupathy <vr258@cornell.edu>
2021-11-03 18:05:08 -0400

    GUI chess board pieces and notation

--- src/gui.mli
+++ src/gui.mli
@@ -4,8 +4,17 @@ val color_rec : int -> int -> int -> bool -> unit;;
 (** [color_rec] draws a rectangle that is either dark or light *)
 
 val draw_matrix : int -> int -> int -> int -> int -> bool -> unit;;
-(** [draw_matrix_rows] draws a matrix with a 
+(** [draw_matrix] draws a matrix with a 
      specified number of rows and columns *)
+
+val notation: int -> int -> int -> unit;;
+(** [notation] creates the algebraic notation for the chess board 
+     ranging from 1-8 on the y-axis and A-H on the x-axis *)
+
+val init_setup: int -> int -> int -> unit;;
+(** [init_setup] initializes the chess board with all the 
+     pieaces *)
 
 val init : unit;;
 (** [init] creates a window and initializes board*)
+
