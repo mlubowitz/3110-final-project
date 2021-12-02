4fb631cceedd5d7effa1d3279d071ce28b7a7a58
Max Lubowitz <maxlubowitz10@gmail.com>
2021-12-01 22:47:28 -0500

    add function in_check_piece and modified all the different check helper functions in state to return the piece that is checking the king
    changed in_check to interpret in_check_piece
    
    added promotion function
    
    added function can_piece_move which is not correct and needs to be fixed
    
    wrote checkpath_list which finds all thbe positions between the king and the piece that is putting it in check

--- src/state.ml
+++ src/state.ml
@@ -140,8 +140,8 @@ let flip_loc = function
 let flip_state st = List.map flip_loc st
 
 (* ==================castle_side======================================== *)
-(*[castle_side] returns the piece on the side that king wantst to castle
-  where the rook would be at the start of the game *)
+(*[castle_side] returns the piece on the side that piece wantst to
+  castle where the rook would be at the start of the game *)
 let castle_side (st : t) (p2 : piece) =
   if get_position p2 = (7, 6) then what_piece st (7, 7)
   else what_piece st (7, 0)
@@ -152,99 +152,295 @@ let castle_side (st : t) (p2 : piece) =
 let diagonal_check_helper (st : t) (p : piece) =
   let a, b = get_position p in
   if a + b <= 7 then
-    diag_check_piece p
-      (piece_in_path st (a, b) (0, a + b))
-      (what_piece st (0, a + b))
-    (*top right diagonal*)
-    || diag_check_piece p
-         (piece_in_path st (a, b) (a + b, 0))
-         (what_piece st (a + b, 0)) (*bottom left diagonal*)
-  else
+    if
+      diag_check_piece p
+        (piece_in_path st (a, b) (0, a + b))
+        (what_piece st (0, a + b))
+      != p
+    then
+      diag_check_piece p
+        (piece_in_path st (a, b) (0, a + b))
+        (what_piece st (0, a + b)) (*top right diagonal*)
+    else if
+      diag_check_piece p
+        (piece_in_path st (a, b) (a + b, 0))
+        (what_piece st (a + b, 0))
+      != p
+    then
+      diag_check_piece p
+        (piece_in_path st (a, b) (a + b, 0))
+        (what_piece st (a + b, 0))
+    else p (*bottom left diagonal*)
+  else if
     diag_check_piece p
       (piece_in_path st (a, b) (7 - (a - b), 7))
       (what_piece st (7 - (a - b), 7))
-    (*top right diagonal*)
-    || diag_check_piece p
-         (piece_in_path st (a, b) (7, a + b - 7))
-         (what_piece st (7, a + b - 7))
+    != p
+  then
+    diag_check_piece p
+      (piece_in_path st (a, b) (7 - (a - b), 7))
+      (what_piece st (7 - (a - b), 7)) (*top right diagonal*)
+  else if
+    diag_check_piece p
+      (piece_in_path st (a, b) (7, a + b - 7))
+      (what_piece st (7, a + b - 7))
+    != p
+  then
+    diag_check_piece p
+      (piece_in_path st (a, b) (7, a + b - 7))
+      (what_piece st (7, a + b - 7))
+  else p
 
 (*bottom left diagonal*)
-let in_check_diagonals (st : t) (p : piece) =
+let in_check_diagonals (st : t) (p : piece) : piece =
   let a, b = get_position p in
   (*row > column to check diagonals for top left and bottom right*)
   if a >= b then
-    (diag_check_piece p
-       (piece_in_path st (a, b) (a - b, 0))
-       (what_piece st (a - b, 0))
-    (*top left diagonal*)
-    || diag_check_piece p
-         (piece_in_path st (a, b) (7, 7 - (a - b)))
-         (what_piece st (7, 7 - (a - b))))
-    (*bottom right diagonal*)
-    || diagonal_check_helper st p
-  else
-    (diag_check_piece p
-       (piece_in_path st (a, b) (0, b - a))
-       (what_piece st (0, b - a))
-    (*top left diagonal*)
-    || diag_check_piece p
-         (piece_in_path st (a, b) (7 - (b - a), 7))
-         (what_piece st (7 - (b - a), 7)))
-    (*bottom right diagonal*)
-    || diagonal_check_helper st p
+    if
+      diag_check_piece p
+        (piece_in_path st (a, b) (a - b, 0))
+        (what_piece st (a - b, 0))
+      != p
+    then
+      diag_check_piece p
+        (piece_in_path st (a, b) (a - b, 0))
+        (what_piece st (a - b, 0)) (*top left diagonal*)
+    else if
+      diag_check_piece p
+        (piece_in_path st (a, b) (7, 7 - (a - b)))
+        (what_piece st (7, 7 - (a - b)))
+      != p
+    then
+      diag_check_piece p
+        (piece_in_path st (a, b) (7, 7 - (a - b)))
+        (what_piece st (7, 7 - (a - b))) (*bottom right diagonal*)
+    else if diagonal_check_helper st p != p then
+      diagonal_check_helper st p
+    else p
+  else if
+    diag_check_piece p
+      (piece_in_path st (a, b) (0, b - a))
+      (what_piece st (0, b - a))
+    != p
+  then
+    diag_check_piece p
+      (piece_in_path st (a, b) (0, b - a))
+      (what_piece st (0, b - a)) (*top left diagonal*)
+  else if
+    diag_check_piece p
+      (piece_in_path st (a, b) (7 - (b - a), 7))
+      (what_piece st (7 - (b - a), 7))
+    != p
+  then
+    diag_check_piece p
+      (piece_in_path st (a, b) (7 - (b - a), 7))
+      (what_piece st (7 - (b - a), 7)) (*bottom right diagonal*)
+  else if diagonal_check_helper st p != p then
+    diagonal_check_helper st p
+  else p
 
 let in_check_orthog_adj (st : t) (p : piece) =
   let a, b = get_position p in
-  orthog_adj_check_piece p
-    (piece_in_path st (a, b) (a, 0))
-    (what_piece st (a, 0))
-  || orthog_adj_check_piece p
-       (piece_in_path st (a, b) (a, 7))
-       (what_piece st (a, 7))
-  || orthog_adj_check_piece p
-       (piece_in_path st (a, b) (0, b))
-       (what_piece st (0, b))
-  || orthog_adj_check_piece p
-       (piece_in_path st (a, b) (7, b))
-       (what_piece st (7, b))
+  if
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (a, 0))
+      (what_piece st (a, 0))
+    != p
+  then
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (a, 0))
+      (what_piece st (a, 0))
+  else if
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (a, 7))
+      (what_piece st (a, 7))
+    != p
+  then
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (a, 7))
+      (what_piece st (a, 7))
+  else if
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (0, b))
+      (what_piece st (0, b))
+    != p
+  then
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (0, b))
+      (what_piece st (0, b))
+  else if
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (7, b))
+      (what_piece st (7, b))
+    != p
+  then
+    orthog_adj_check_piece p
+      (piece_in_path st (a, b) (7, b))
+      (what_piece st (7, b))
+  else p
 
 let in_check_knight (st : t) (p : piece) =
   let a, b = get_position p in
-  (a > 1 && b > 0 && knight_check_piece p (what_piece st (a - 2, b - 1)))
-  || a > 1 && b < 7
-     && knight_check_piece p (what_piece st (a - 2, b + 1))
-  || a > 0 && b > 1
-     && knight_check_piece p (what_piece st (a - 1, b - 2))
-  || a > 0 && b < 6
-     && knight_check_piece p (what_piece st (a - 1, b + 2))
-  || a < 7 && b > 1
-     && knight_check_piece p (what_piece st (a + 1, b - 2))
-  || a < 7 && b < 6
-     && knight_check_piece p (what_piece st (a + 1, b + 2))
-  || a < 6 && b > 0
-     && knight_check_piece p (what_piece st (a + 2, b - 1))
-  || a < 6 && b < 7
-     && knight_check_piece p (what_piece st (a + 2, b + 1))
+  if
+    a > 1 && b > 0
+    && knight_check_piece p (what_piece st (a - 2, b - 1)) != p
+  then knight_check_piece p (what_piece st (a - 2, b - 1))
+  else if
+    a > 1 && b < 7
+    && knight_check_piece p (what_piece st (a - 2, b + 1)) != p
+  then knight_check_piece p (what_piece st (a - 2, b + 1))
+  else if
+    a > 0 && b > 1
+    && knight_check_piece p (what_piece st (a - 1, b - 2)) != p
+  then knight_check_piece p (what_piece st (a - 1, b - 2))
+  else if
+    a > 0 && b < 6
+    && knight_check_piece p (what_piece st (a - 1, b + 2)) != p
+  then knight_check_piece p (what_piece st (a - 1, b + 2))
+  else if
+    a < 7 && b > 1
+    && knight_check_piece p (what_piece st (a + 1, b - 2)) != p
+  then knight_check_piece p (what_piece st (a + 1, b - 2))
+  else if
+    a < 7 && b < 6
+    && knight_check_piece p (what_piece st (a + 1, b + 2)) != p
+  then knight_check_piece p (what_piece st (a + 1, b + 2))
+  else if
+    a < 6 && b > 0
+    && knight_check_piece p (what_piece st (a + 2, b - 1)) != p
+  then knight_check_piece p (what_piece st (a + 2, b - 1))
+  else if
+    a < 6 && b < 7
+    && knight_check_piece p (what_piece st (a + 2, b + 1)) != p
+  then knight_check_piece p (what_piece st (a + 2, b + 1))
+  else p
 
-let in_check (st : t) (p : piece) =
-  in_check_diagonals st p
-  || in_check_orthog_adj st p
-  || in_check_knight st p
+let in_check_piece (st : t) (p : piece) =
+  let diagP = in_check_diagonals st p in
+  let orthoAdjP = in_check_orthog_adj st p in
+  let knightP = in_check_knight st p in
+  if diagP != p then diagP
+  else if orthoAdjP != p then orthoAdjP
+  else if knightP != p then knightP
+  else p
+
+let in_check (st : t) (p : piece) = in_check_piece st p != p
 
 (* ==================find_king======================================== *)
 let rec find_king t color =
   match t with
-  | [] -> failwith "King should be in board."
+  | [] -> failwith "piece should be in board."
   | (l, p) :: t ->
-      if is_king p && color = get_color p then l else find_king t color
-
-(* ==================possible_moves======================================== *)
-let rec possible_moves t color =
-  match t with
-  | [] -> failwith "King should be in board."
-  | (l, p) :: t ->
-      if is_king p && color = get_color p then l else find_king t color
+      if get_piece_type p = "K" && color = get_color p then l
+      else find_king t color
 
 (* ==================promotion======================================== *)
 let promotion (st : t) (p : piece) (pdest : int * int) =
-  is_pawn p && fst pdest = 0
+  get_piece_type p = "P" && fst pdest = 0
+
+(* ==================possible_moves======================================== *)
+let rec possible_moves t color = "unimplemented"
+
+(* ==================stalemate============================================= *)
+
+let move_up1_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst - 1, pSnd) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let move_down1_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst + 1, pSnd) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let move_left1_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst, pSnd - 1) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let move_right1_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst, pSnd + 1) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let move_URdiag_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst - 1, pSnd + 1) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let move_LRdiag_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst + 1, pSnd + 1) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let move_ULdiag_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst - 1, pSnd - 1) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let move_LLdiag_helper (st : t) (p1 : piece) (pFst : int) (pSnd : int) =
+  let p2 = what_piece st (pFst + 1, pSnd - 1) in
+  is_legal p1 p2 && in_check st p2 = false
+
+let can_not_knight_move (st : t) (p1 : piece) =
+  let pFst, pSnd = get_position p1 in
+  move_up1_helper st p1 pFst pSnd
+  || move_down1_helper st p1 pFst pSnd
+  || move_left1_helper st p1 pFst pSnd
+  || move_right1_helper st p1 pFst pSnd
+  || move_URdiag_helper st p1 pFst pSnd
+  || move_LRdiag_helper st p1 pFst pSnd
+  || move_ULdiag_helper st p1 pFst pSnd
+  || move_LLdiag_helper st p1 pFst pSnd
+
+let can_knight_move (st : t) (knight : piece) =
+  let a, b = get_position knight in
+  is_legal knight (what_piece st (a - 2, b - 1))
+  || is_legal knight (what_piece st (a - 2, b + 1))
+  || is_legal knight (what_piece st (a - 1, b - 2))
+  || is_legal knight (what_piece st (a - 1, b + 2))
+  || is_legal knight (what_piece st (a + 1, b - 2))
+  || is_legal knight (what_piece st (a + 1, b + 2))
+  || is_legal knight (what_piece st (a + 2, b - 1))
+  || is_legal knight (what_piece st (a + 2, b + 1))
+
+let can_piece_move (st : t) (p : piece) =
+  if get_piece_type p != "N" then can_not_knight_move st p
+  else can_knight_move st p
+
+let rec stalemate t color =
+  match t with
+  | [] -> true
+  | (l, p) :: t ->
+      if color = get_color p && can_piece_move t p then false
+      else stalemate t color
+
+(* ==================checkmate======================================== *)
+
+let is_inbetween l p1 p2 c1 c2 =
+  fst l < max p1 c1
+  && fst l > min p1 c1
+  && snd l < max p2 c2
+  && snd l > min p2 c2
+
+let rec path_list_acc t acc p1 p2 c1 c2 =
+  match t with
+  | [] -> acc
+  | (l, p) :: t ->
+      if is_inbetween l p1 p2 c1 c2 = false then
+        path_list_acc t acc p1 p2 c1 c2
+      else if diagonal_move (p1, p2) (c1, c2) then
+        if diagonal_move (p1, p2) l then
+          path_list_acc t (l :: acc) p1 p2 c1 c2
+        else path_list_acc t acc p1 p2 c1 c2
+      else if horizontal_move (p1, p2) (c1, c2) then
+        if horizontal_move (p1, p2) l then
+          path_list_acc t (l :: acc) p1 p2 c1 c2
+        else path_list_acc t acc p1 p2 c1 c2
+      else if verticle_move (p1, p2) (c1, c2) then
+        if verticle_move (p1, p2) l then
+          path_list_acc t (l :: acc) p1 p2 c1 c2
+        else path_list_acc t acc p1 p2 c1 c2
+      else path_list_acc t acc p1 p2 c1 c2
+
+let path_list t c1 c2 p1 p2 = path_list_acc t [ (c1, c2) ] p1 p2 c1 c2
+
+let checkpath_list (st : t) (p : piece) =
+  let checkPiece = in_check_piece st p in
+  let c1, c2 = get_position checkPiece in
+  let p1, p2 = get_position p in
+  if get_piece_type checkPiece = "N" then []
+  else path_list st p1 p2 c1 c2
